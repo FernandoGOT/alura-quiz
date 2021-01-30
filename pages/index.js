93e5cbb20773c7f686763f76f6433a1c63c50c54
FernandoGOT <fernando.1f@hotmail.com>
2021-01-30 18:58:36 -0300

    Created the widget component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 
 import db from '../db.json'
+import Widget from '../src/components/Widget'
 
 const BackgroundImage = styled.div`
   flex: 1;
@@ -9,10 +10,36 @@ const BackgroundImage = styled.div`
   background-image: url(${db.bg});
 `
 
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`
+
 export default function Home() {
   return (
     <BackgroundImage>
-      auhauhauhauhuahauhua
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>{db.description}</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Content>
+            <h1>ttitle 2</h1>
+            <p>text 2</p>
+          </Widget.Content>
+        </Widget>
+      </QuizContainer>
     </BackgroundImage>
   )
 }
