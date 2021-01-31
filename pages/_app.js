43db2a62216c8dab7e4e10521c66d4da5ae38e7c
FernandoGOT <fernando.1f@hotmail.com>
2021-01-31 00:50:50 -0300

    Added fonts and og:image

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 import db from '../db.json'
@@ -32,6 +33,11 @@ const theme = db.theme
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta property="og:image" content={db.bg} />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
+      </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
